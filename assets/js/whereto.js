ea6206488bfbbca493255f4193889114e20a9da0
Angelo Cordon <angelocordon@gmail.com>
2015-09-10 13:26:18 -0700

    working on angular

--- assets/js/whereto.js
+++ assets/js/whereto.js
@@ -1,1 +1,20 @@
-var app = angular.module('whereto', []);
+(function(){
+  var app = angular.module('whereto', [], function ($interpolateProvider) {
+    $interpolateProvider.startSymbol('[[');
+    $interpolateProvider.endSymbol(']]');
+  });
+
+  var latestLocations = [
+    'San Francisco',
+    'Seattle',
+    'Arizona',
+    'San Diego'
+  ];
+
+  app.controller('locatorController', function(){
+    this.locations = latestLocations;
+  });
+
+
+
+})();
